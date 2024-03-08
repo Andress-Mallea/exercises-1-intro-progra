affcb434d7ff2d39a81857566dd4cce077c82dab
Andress-Mallea <159574476+Andress-Mallea@users.noreply.github.com>
2024-03-08 17:05:42 -0400

    prueba4

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void exercise_1(string s1, string s2, string s3) {
   // TODO: YOUR CODE HERE
+  cin >> s1 >> s2 >> s3;
         if (s1 < s2 && s1 < s3)
         {
             cout << s1 << endl;
