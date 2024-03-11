a627a58967466fff98d72f5aba623d206af183c0
Andress-Mallea <159574476+Andress-Mallea@users.noreply.github.com>
2024-03-10 22:56:59 -0400

    prueba 2 ejercicio 6

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -92,17 +92,19 @@ void exercise_5(char character) {
 
 void exercise_6(int number) {
   // TODO: YOUR CODE HERE
+<<<<<<< HEAD
    switch(number)
   {
-      case 1: cout << "Lunes" << endl; break;
-      case 2: cout << "Martes" << endl; break;
-      case 3: cout << "Miercoles" << endl; break;
-      case 4: cout << "Jueves" << endl; break;
-      case 5: cout << "Viernes" << endl; break;
-      case 6: cout << "Sabado" << endl; break;
-      case 7: cout << "Domingo" << endl; break;
+      case 1: cout << "Monday" << endl; break;
+      case 2: cout << "Tuesday" << endl; break;
+      case 3: cout << "Wednsday" << endl; break;
+      case 4: cout << "Thursday" << endl; break;
+      case 5: cout << "Friday" << endl; break;
+      case 6: cout << "Saturday" << endl; break;
+      case 7: cout << "Sunday" << endl; break;
       default: cout << "Invalid input" << endl; break;
   }
+
 }
 
 void exercise_7(double r) {
